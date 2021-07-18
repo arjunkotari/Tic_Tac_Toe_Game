522d36eb0a95d558a83fdd74fad253684a56102e
arjunkotari <arjun.kotari123@gmail.com>
2021-07-18 19:19:17 +0530

    Added:Code to determine after every move the winner or tie

--- src/Tic_Tac_Toe/TicTacToe.java
+++ src/Tic_Tac_Toe/TicTacToe.java
@@ -1,9 +1,14 @@
 package Tic_Tac_Toe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class TicTacToe {
 	
+	static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+	static ArrayList<Integer> systemPositions = new ArrayList<Integer>();
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game...");
@@ -32,6 +37,8 @@ public class TicTacToe {
 		//calling showBoard method
 		
 		makeMove(gameBoard,turn,position);
+		String result = checkWinner();
+		System.out.println(result);
 				
 		
 	}
@@ -60,100 +67,144 @@ public class TicTacToe {
 		else if(turn == 0) {
 			turn =1;
 		}
-		
-		if(turn % 2 == 0) {
-			System.out.println("Player turn...");	
-			move = 'X';
-			System.out.println("Enter your position 1-9");
-			position = Utility.getUserInteger();
+		//change turn
+		for(int i=1; i<=9; i++)
+		{
+			if(turn % 2 == 0) {
+				System.out.println("Player turn...");	
+				move = 'X';
+				System.out.println("Enter your position 1-9");
+				position = Utility.getUserInteger();
+				playerPositions.add(position);
+			}
+			else {
+				System.out.println("System turn...");
+				move = 'O';
+				Random r1 = new Random();
+				position = r1.nextInt(9)+1;	
+				systemPositions.add(position);
+			}
+			turn++;
+			
+			
+			switch(position) {
+			case 1:
+				if(gameBoard[0][0] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[0][0] = move;					
+				}	
+				else {
+					System.out.println("Position full...");
+				}
+					
+				break;
+			case 2:
+				if(gameBoard[0][2] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[0][2] = move;
+				}	
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			case 3:
+				if(gameBoard[0][4] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[0][4] = move;
+				}		
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			case 4:
+				if(gameBoard[2][0] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[2][0] = move;
+				}	
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			case 5:
+				if(gameBoard[2][2] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[2][2] = move;
+				}	
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			case 6:
+				if(gameBoard[2][4] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[2][4] = move;
+				}		
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			case 7:
+				if(gameBoard[4][0] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[4][0] = move;
+				}		
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			case 8:
+				if(gameBoard[4][2] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[4][2] = move;
+				}
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			case 9:
+				if(gameBoard[4][4] != 'X' && gameBoard[0][0] != 'O') {
+					gameBoard[4][4] = move;
+				}		
+				else {
+					System.out.println("Position full...");
+				}
+				break;
+			default:
+				break;
+			}
+			showBoard(gameBoard);
 		}
-		else {
-			System.out.println("System turn...");
-			move = 'O';
-			Random r1 = new Random();
-			position = r1.nextInt(9)+1;			
-		}
-		turn++;
 		
 		
-		switch(position) {
-		case 1:
-			if(gameBoard[0][0] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[0][0] = move;					
-			}	
-			else {
-				System.out.println("Position full...");
+	}
+	/*
+	 * check winner
+	 */
+	public static String checkWinner() {
+		List topRow = Arrays.asList(1,2,3);
+		List middleRow = Arrays.asList(4, 5, 6);
+		List bottomRow = Arrays.asList(7, 8, 9);
+		List leftCol = Arrays.asList(1, 4, 7);
+		List middleCol = Arrays.asList(2, 5, 8);
+		List rightCol = Arrays.asList(3, 6, 9);
+		List cross1 = Arrays.asList(1, 5, 9);
+		List cross2 =Arrays.asList(3, 5, 7);
+		
+		List<List> winning = new ArrayList<List>();
+		winning.add(topRow);
+		winning.add(middleRow);
+		winning.add(bottomRow);
+		winning.add(leftCol);
+		winning.add(middleCol);
+		winning.add(rightCol);
+		winning.add(cross1);
+		winning.add(cross2);
+		
+		for(List l : winning) {
+			if(playerPositions.containsAll(l)) {
+				return "congrats you won";
 			}
-				
-			break;
-		case 2:
-			if(gameBoard[0][2] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[0][2] = move;
-			}	
-			else {
-				System.out.println("Position full...");
+			else if(systemPositions.containsAll(l)) {
+				return "System won";
 			}
-			break;
-		case 3:
-			if(gameBoard[0][4] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[0][4] = move;
-			}		
-			else {
-				System.out.println("Position full...");
+			else if(playerPositions.size() + systemPositions.size() == 9) {
+				return "Tie";
 			}
-			break;
-		case 4:
-			if(gameBoard[2][0] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[2][0] = move;
-			}	
-			else {
-				System.out.println("Position full...");
-			}
-			break;
-		case 5:
-			if(gameBoard[2][2] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[2][2] = move;
-			}	
-			else {
-				System.out.println("Position full...");
-			}
-			break;
-		case 6:
-			if(gameBoard[2][4] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[2][4] = move;
-			}		
-			else {
-				System.out.println("Position full...");
-			}
-			break;
-		case 7:
-			if(gameBoard[4][0] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[4][0] = move;
-			}		
-			else {
-				System.out.println("Position full...");
-			}
-			break;
-		case 8:
-			if(gameBoard[4][2] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[4][2] = move;
-			}
-			else {
-				System.out.println("Position full...");
-			}
-			break;
-		case 9:
-			if(gameBoard[4][4] != 'X' && gameBoard[0][0] != 'O') {
-				gameBoard[4][4] = move;
-			}		
-			else {
-				System.out.println("Position full...");
-			}
-			break;
-		default:
-			break;
 		}
-		showBoard(gameBoard);
+		
+		return "";
 	}
 	/*
 	 * creating a method to display the board
