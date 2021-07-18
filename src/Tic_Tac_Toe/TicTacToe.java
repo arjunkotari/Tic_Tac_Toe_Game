0e4fffebbfc611966b7278ea1ecb8a1441767065
arjunkotari <arjun.kotari123@gmail.com>
2021-07-18 20:07:51 +0530

    Added:Computer getting its turn

--- src/Tic_Tac_Toe/TicTacToe.java
+++ src/Tic_Tac_Toe/TicTacToe.java
@@ -1,14 +1,8 @@
 package Tic_Tac_Toe;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 public class TicTacToe {
-	
-	static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
-	static ArrayList<Integer> systemPositions = new ArrayList<Integer>();
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game...");
@@ -34,12 +28,8 @@ public class TicTacToe {
 		}
 		turn %= 2;		
 		playerMove = allowPlayer(turn,playerMove,toss);
-		//calling showBoard method
-		
-		makeMove(gameBoard,turn,position);
-		String result = checkWinner();
-		System.out.println(result);
-				
+		//calling showBoard method		
+		makeMove(gameBoard,turn,position,board);
 		
 	}
 	/*
@@ -58,7 +48,7 @@ public class TicTacToe {
 	 * placing moves into specific position
 	 * If the place is free then place the X
 	 */
-	public static void makeMove(char[][] gameBoard,int turn,int position) {
+	public static void makeMove(char[][] gameBoard,int turn,int position, char[] board) {
 		char move;
 		if(turn == 1)
 		{
@@ -75,14 +65,14 @@ public class TicTacToe {
 				move = 'X';
 				System.out.println("Enter your position 1-9");
 				position = Utility.getUserInteger();
-				playerPositions.add(position);
+				//playerPositions.add(position);
 			}
 			else {
 				System.out.println("System turn...");
 				move = 'O';
 				Random r1 = new Random();
 				position = r1.nextInt(9)+1;	
-				systemPositions.add(position);
+				//systemPositions.add(position);
 			}
 			turn++;
 			
@@ -90,122 +80,210 @@ public class TicTacToe {
 			switch(position) {
 			case 1:
 				if(gameBoard[0][0] != 'X' && gameBoard[0][0] != 'O') {
-					gameBoard[0][0] = move;					
+					gameBoard[0][0] = move;
+					board[1]=move;
 				}	
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 					
 				break;
 			case 2:
-				if(gameBoard[0][2] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[0][2] != 'X' && gameBoard[0][2] != 'O') {
 					gameBoard[0][2] = move;
+					board[2]=move;
 				}	
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			case 3:
-				if(gameBoard[0][4] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[0][4] != 'X' && gameBoard[0][4] != 'O') {
 					gameBoard[0][4] = move;
+					board[3]=move;
 				}		
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			case 4:
-				if(gameBoard[2][0] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[2][0] != 'X' && gameBoard[2][0] != 'O') {
 					gameBoard[2][0] = move;
+					board[4]=move;
 				}	
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			case 5:
-				if(gameBoard[2][2] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[2][2] != 'X' && gameBoard[2][2] != 'O') {
 					gameBoard[2][2] = move;
+					board[5]=move;
 				}	
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			case 6:
-				if(gameBoard[2][4] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[2][4] != 'X' && gameBoard[2][4] != 'O') {
 					gameBoard[2][4] = move;
+					board[6]=move;
 				}		
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			case 7:
-				if(gameBoard[4][0] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[4][0] != 'X' && gameBoard[4][0] != 'O') {
 					gameBoard[4][0] = move;
+					board[7]=move;
 				}		
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			case 8:
-				if(gameBoard[4][2] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[4][2] != 'X' && gameBoard[4][2] != 'O') {
 					gameBoard[4][2] = move;
+					board[8]=move;
 				}
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			case 9:
-				if(gameBoard[4][4] != 'X' && gameBoard[0][0] != 'O') {
+				if(gameBoard[4][4] != 'X' && gameBoard[4][4] != 'O') {
 					gameBoard[4][4] = move;
+					board[9]=move;
 				}		
 				else {
 					System.out.println("Position full...");
+					i--;
+					turn--;
 				}
 				break;
 			default:
 				break;
 			}
 			showBoard(gameBoard);
+			/*
+			 * check winner
+			 */
+			if(gameBoard[0][0]=='X' && gameBoard[0][2]=='X' && gameBoard[0][4]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			else if(gameBoard[2][0]=='X' && gameBoard[2][2]=='X' && gameBoard[2][4]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			else if(gameBoard[4][0]=='X' && gameBoard[4][2]=='X' && gameBoard[4][4]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			
+			else if(gameBoard[0][0]=='X' && gameBoard[2][0]=='X' && gameBoard[4][0]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			else if(gameBoard[0][2]=='X' && gameBoard[2][2]=='X' && gameBoard[4][2]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			else if(gameBoard[0][4]=='X' && gameBoard[2][4]=='X' && gameBoard[4][4]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			else if(gameBoard[0][0]=='X' && gameBoard[2][2]=='X' && gameBoard[4][4]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			else if(gameBoard[4][0]=='X' && gameBoard[2][2]=='X' && gameBoard[0][4]=='X' )
+			{
+				System.out.println("You own the game");
+				break;
+			}
+			
+			
+			
+			
+			
+			else if(gameBoard[0][0]=='O' && gameBoard[0][2]=='O' && gameBoard[0][4]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			else if(gameBoard[2][0]=='O' && gameBoard[2][2]=='O' && gameBoard[2][4]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			else if(gameBoard[4][0]=='O' && gameBoard[4][2]=='O' && gameBoard[4][4]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			
+			else if(gameBoard[0][0]=='O' && gameBoard[2][0]=='O' && gameBoard[4][0]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			else if(gameBoard[0][2]=='O' && gameBoard[2][2]=='O' && gameBoard[4][2]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			else if(gameBoard[0][4]=='O' && gameBoard[2][4]=='O' && gameBoard[4][4]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			else if(gameBoard[0][0]=='O' && gameBoard[2][2]=='O' && gameBoard[4][4]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			else if(gameBoard[4][0]=='O' && gameBoard[2][2]=='O' && gameBoard[0][4]=='O' )
+			{
+				System.out.println("System own the game");
+				break;
+			}
+			else if(i == 9)
+			{
+				System.out.println("Game Tie...");
+				break;
+			}
+			
 		}
 		
 		
 	}
-	/*
-	 * check winner
-	 */
-	public static String checkWinner() {
-		List topRow = Arrays.asList(1,2,3);
-		List middleRow = Arrays.asList(4, 5, 6);
-		List bottomRow = Arrays.asList(7, 8, 9);
-		List leftCol = Arrays.asList(1, 4, 7);
-		List middleCol = Arrays.asList(2, 5, 8);
-		List rightCol = Arrays.asList(3, 6, 9);
-		List cross1 = Arrays.asList(1, 5, 9);
-		List cross2 =Arrays.asList(3, 5, 7);
-		
-		List<List> winning = new ArrayList<List>();
-		winning.add(topRow);
-		winning.add(middleRow);
-		winning.add(bottomRow);
-		winning.add(leftCol);
-		winning.add(middleCol);
-		winning.add(rightCol);
-		winning.add(cross1);
-		winning.add(cross2);
-		
-		for(List l : winning) {
-			if(playerPositions.containsAll(l)) {
-				return "congrats you won";
-			}
-			else if(systemPositions.containsAll(l)) {
-				return "System won";
-			}
-			else if(playerPositions.size() + systemPositions.size() == 9) {
-				return "Tie";
-			}
-		}
-		
-		return "";
-	}
+	
+	
 	/*
 	 * creating a method to display the board
 	 */
