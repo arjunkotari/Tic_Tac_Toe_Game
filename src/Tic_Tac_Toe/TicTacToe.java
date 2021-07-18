f13f7521454fc9e46703f7b9c81af4a1dff0515a
arjunkotari <arjun.kotari123@gmail.com>
2021-07-18 14:35:17 +0530

    Added:player to input X or O

--- src/Tic_Tac_Toe/TicTacToe.java
+++ src/Tic_Tac_Toe/TicTacToe.java
@@ -1,5 +1,7 @@
 package Tic_Tac_Toe;
 
+import java.util.Random;
+
 public class TicTacToe {
 	/*
 	 * creating method with size 10
@@ -16,7 +18,18 @@ public class TicTacToe {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game...");
+		int user=1;
 		//calling createBoard method in main method		 
 		char[] board = createBoard();
+allowPlayer();
+		
+	}
+	/*
+	 * Creating method to allow player to input X or O
+	 */
+	public static char allowPlayer() {
+		System.out.println("Please enter X or O");
+		char playerMove = Utility.getUserChar();
+		return playerMove;
 	}
 }
