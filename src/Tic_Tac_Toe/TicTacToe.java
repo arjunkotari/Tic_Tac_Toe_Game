4c63274524e0b46173d39d5175a32392fc1c520b
arjunkotari <arjun.kotari123@gmail.com>
2021-07-18 15:24:13 +0530

    Added:index from 1 to 9 to make the move

--- src/Tic_Tac_Toe/TicTacToe.java
+++ src/Tic_Tac_Toe/TicTacToe.java
@@ -18,23 +18,68 @@ public class TicTacToe {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game...");
-		int user=1;
-		//calling createBoard method in main method		 
-		char[] board = createBoard();
-		allowPlayer();
-		//calling showBoard method
-		showBoard();
-		
-	}
-	/*
-	 * creating a method to display the board
-	 */
-	public static void showBoard() {
+		int user=1,position;
 		char[][] gameBoard = {{' ','|',' ','|',' '},
 				{'-','+','-','+','-'},
 				{' ','|',' ','|',' '},
 				{'-','+','-','+','-'},
 				{' ','|',' ','|',' '}};
+		//calling createBoard method in main method		 
+		showBoard(gameBoard);
+		char[] board = createBoard();
+		allowPlayer();
+		//calling showBoard method
+		System.out.println("Enter your place 1-9");
+		position = Utility.getUserInteger();
+		makeMove(gameBoard, position);
+		showBoard(gameBoard);
+		
+		
+		
+		
+	}
+	/*
+	 * placing moves into specific position
+	 */
+	public static void makeMove(char[][] gameBoard, int position) {
+
+		switch(position) {
+		case 1:
+			gameBoard[0][0] = 'X';
+			break;
+		case 2:
+			gameBoard[0][2] = 'X';
+			break;
+		case 3:
+			gameBoard[0][4] = 'X';
+			break;
+		case 4:
+			gameBoard[2][0] = 'X';
+			break;
+		case 5:
+			gameBoard[2][2] = 'X';
+			break;
+		case 6:
+			gameBoard[2][4] = 'X';
+			break;
+		case 7:
+			gameBoard[4][0] = 'X';
+			break;
+		case 8:
+			gameBoard[4][2] = 'X';
+			break;
+		case 9:
+			gameBoard[4][4] = 'X';
+			break;
+		default:
+			break;
+		}
+	}
+	/*
+	 * creating a method to display the board
+	 */
+	public static void showBoard(char[][] gameBoard) {
+		
 		for(char[] row:gameBoard) {
 			for(char display:row) {
 				System.out.print(display);
